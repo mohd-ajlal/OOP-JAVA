f8935049d419225cb3b8ff39a24c96a6b0ba7dea
Mohd Ajlal <ajlal00786@gmail.com>
2023-09-10 01:32:51 +0530

    OOP

---
+++ code/src/com/mohdajlal/introduction/Main.java
@@ -0,0 +1,23 @@
+package com.mohdajlal.introduction;
+
+public class Main {
+    public static void main(String[] args) {
+//        stores 5 roll no.
+        int[] arr = new int[5];
+
+//        stores 5 name
+        String[] names = new String[5];
+
+//        Data of 5 students {roll no., name, marks}
+        Student[] students = new Student[5];
+        Student ajlal;
+    }
+
+
+}
+//    create a class
+class Student{
+    int[] roll = new int[5];
+    String[] name = new String[5];
+    float[] marks = new float[5];
+}
