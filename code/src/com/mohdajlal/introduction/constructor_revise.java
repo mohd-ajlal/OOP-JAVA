21bef92f5f7aee694c769539d582f9829d720601
Mohd Ajlal <ajlal00786@gmail.com>
2023-11-23 18:16:53 +0530

    OOP

---
+++ code/src/com/mohdajlal/introduction/constructor_revise.java
@@ -0,0 +1,17 @@
+package com.mohdajlal.introduction;
+
+public class constructor_revise {
+
+    public static void main(String[] args) {
+        constructor_r cr = new constructor_r(); // constructors executes automatically when we create an object
+        System.out.println(cr.x);
+    }
+}
+
+class constructor_r{
+    int x;
+    public constructor_r(){
+        System.out.println("Hello");
+        x = 5;
+    }
+}
